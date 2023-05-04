b46c654044416467b02b7f5914f1b7caa6d9b027
Rebecca Lehew <lehew316@gmail.com>
2023-05-04 13:14:00 -0500

    Finished code for generateHTML.js
    - Added Bootstrap styling for each role in HTML
    - Added function to push object to the page
    - Added function to join the data
    - Added function to return the HTML page with the new team data

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,35 +1,124 @@
-// generate main HTML page
-const generateTeamPage = function (employeeCards) {
+// create manager profile
+const createManager = function (manager) {
     return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Profile</title>
-    <link href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" rel="stylesheet" />
-</head>
+    <div class='col-4 mt-4>
+        <div class='card h-100 border-primary'>
+            <div class='card-header bg-primary'>
+                <h3 class='name'>${manager.name}</h3>
+                <h4 class='role'><i class='fa-solid fa-users' style='color: #ffffff;'></i>Manager</h4>
+            </div>
 
-<body>
-    <nav class="navbar navbar-dark bg-primary">
-        <span class="navbar-brand" href="#">
-            <i class="fa-sharp fa-solid fa-people-group" style="color: #ffffff;"</i>Bootstrap
-        </span>
-    </nav>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
-</html>
-    `
+            <div class='card-body text-primary'>
+                <p class='id>ID: ${manager.id}</p>
+                <p class='email'>Email: <a href='mailto:${manager.email}'>${manager.email}</a></p>
+                <p class='office-number'>Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
 }
 
-// create Engineer card
-// create Intern card
-// create Manager card
+// create Engineer profile
+const createEngineer = function (engineer) {
+    return `
+    <div class='col-4 mt-4>
+        <div class='card h-100 border-primary'>
+            <div class='card-header bg-primary'>
+                <h3 class='name'>${engineer.name}</h3>
+                <h4 class='role'><i class='fa-solid fa-user-gear' style='color: #ffffff;'></i>Engineer</h4>
+            </div>
+
+            <div class='card-body text-primary'>
+                <p class='id>ID: ${engineer.id}</p>
+                <p class='email'>Email: <a href='mailto:${engineer.email}'>${engineer.email}</a></p>
+                <p class='github'>GitHub: ${engineer.github}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+// create Intern profile
+const createIntern = function (intern) {
+    return `
+    <div class='col-4 mt-4>
+        <div class='card h-100 border-primary'>
+            <div class='card-header bg-primary'>
+                <h3 class='name'>${intern.name}</h3>
+                <h4 class='role'><i class='fa-solid fa-user-graduate' style='color: #ffffff;'></i>Intern</h4>
+            </div>
+
+            <div class='card-body text-primary'>
+                <p class='id>ID: ${intern.id}</p>
+                <p class='email'>Email: <a href='mailto:${intern.email}'>${intern.email}</a></p>
+                <p class='school'>School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
 // push array object to page
-// join
-// return new data to page
+generateHTML = (data) => {
+    pageArray = [];
+
+    for (let i=0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            const managerCard = createManager(employee);
+            pageArray.push(managerCard);
+        } else if (role === 'Engineer') {
+            const engineerCard = createEngineer(employee);
+            pageArray.push(engineerCard);
+        } else if (role === 'Intern') {
+            const internCard = createIntern(employee);
+            pageArray.push(internCard);
+        }
+    }
+
+    // join data
+    const employeeCards = pageArray.join('')
+    const createTeam = createTeamPage(employeeCards);
+    return createTeam();
+}
+
+// create team HTML page
+const createTeamPage = function (employeeCards) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en" data-bs-theme="light">
+    <head>
+        <meta charset='UTF-8'>
+        <link rel='stylesheet' href='https://cdn.jsdeliver.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'>
+        <link rel='stylesheet' href='style.css' />
+        <script src='https://kit.fontawesome.com/57383aac0c.js' crossorigin='anonymous'></script>
+    </head>
+
+    <body>
+        <header>
+            <nav class='navbar bg-primary' id='navbar'>
+                <span class='navbar-brand mb-0 h1 w-100 text-center' id='navbar-text'><i class="fa-regular fa-cloud-moon" style="color: #ffffff;"></i> The Dream Team!</span>
+            </nav>
+        <header>
+
+        <main>
+            <div class='container'>
+                <div class='row justify-content-center' id='employee-cards'>
+                    
+                    ${employeeCards}
+
+                </div>
+            </div>
+        </main>
+
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js' integrity='sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
+    </body>
+
+    </html>
+    `;
+}
+
+// export to index.html
+module.exports = generateHTML;
